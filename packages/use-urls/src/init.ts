860fc3853e4bd16e1a5d3eddc22333f45685168b
leomerida15 <dimasmerida15@gmail.com>
2025-06-21 21:01:45 -0400

    v0.8.8

--- packages/use-urls/src/init.ts
+++ packages/use-urls/src/init.ts
@@ -5,6 +5,7 @@ import { existsSync, writeFileSync } from "node:fs";
 import { hookTemp } from "./hook";
 import { indexTemp } from "./indexTemp";
 import { execSync } from "node:child_process";
+import { SsrTemp } from "./ssr";
 
 export const GenerateCommand = (program: Command) => {
   program
@@ -29,13 +30,52 @@ export const GenerateCommand = (program: Command) => {
 
         const indexPath = join(baseUrl, 'index.ts');
 
-        new UrlsBuild("app", baseUrl, ignore).create();
+        const objPath = join(baseUrl, 'index.ts');
 
-        if(!str.omit) 
+        const ssrPath = join(baseUrl, 'index.ts');
+
+        const urlsBuild = new UrlsBuild("app", baseUrl, ignore)
+        
+        urlsBuild.create();
           
+        writeFileSync(join(objPath, "urls.obj.ts"), `export const UrlsObj = ${JSON.stringify(urlsBuild.obj)};`);
+        writeFileSync(join(ssrPath, "urls.ssr.ts"), SsrTemp);
         writeFileSync(hookPath, hookTemp);
-
         writeFileSync(indexPath, indexTemp);
+        
+
+      } catch (error) {
+        const err = error as Error;
+
+        console.error("Error:", err.message);
+      }
+    });
+
+  program
+    .command("update")
+    .description("re generate files")
+    .option("-p, --path <char>", "path by save result", ".")
+    .option("-i, --ignore <char>", "item,item,item ...", ",")
+    .option("-o, --omit", "omit hook", false)
+    .action((str) => {
+      try {
+        
+        const ignore = str.ignore.split(",");
+
+        const baseUrl = join(str.path, "urls");
+
+        if(!existsSync(baseUrl)) {
+          execSync(`mkdir ${baseUrl}`, { stdio: "pipe" });
+        }
+
+        const objPath = join(baseUrl, 'index.ts');
+
+        const urlsBuild = new UrlsBuild("app", baseUrl, ignore)
+        
+        urlsBuild.create();
+          
+        writeFileSync(join(objPath, "urls.obj.ts"), `export const UrlsObj = ${JSON.stringify(urlsBuild.obj)};`);
+
       } catch (error) {
         const err = error as Error;
 
